b010e55088ed42f7b81ec08c64659afe06060559
zidz <zidios101@gmail.com>
2020-04-20 22:42:04 +0800

    Initial Commit

---
+++ eScooter Tracker MapKit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  eScooter Tracker MapKit
+//
+//  Created by Yazid on 20/04/2020.
+//  Copyright © 2020 UiTM Kampus Samarahan Cawangan Sarawak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
